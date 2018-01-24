b6be62863674eb3cedba003a74d091dd56c9a185
shankar-spotsoon <shankar@spotsoon.com>
2018-01-24 11:07:43 +0530

    fabric

--- app/src/main/java/com/apextechies/kmaaoapp/MainActivity.kt
+++ app/src/main/java/com/apextechies/kmaaoapp/MainActivity.kt
@@ -5,13 +5,19 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 import kotlinx.android.synthetic.main.activity_main.*
+//import sun.security.krb5.internal.KDCOptions.with
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
